7f8c8745439170cc7aa5f6d1d22a05a77cd43cb2
MBrian Website <muhammad.brian.27@gmail.com>
2023-01-15 19:37:00 +0700

    SET UP SEARCH BAR

--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -1,8 +1,21 @@
+<script setup>
+const city = ref("")
+const cityError = ref(false)
+
+const handleSearch = () => {
+    if (!city.value) {
+        return cityError.value = true
+    } else {
+        navigateTo(`/city/${city}/car`)
+    }
+}
+</script>
+
 <template>
-    <div
-        class="font-serif w-full text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
+    <div class="font-serif w-full text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
+        :class="cityError ? 'ring-8 ring-red-600' : ''">
         <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
-            placeholder="Search by city..." />
-        <button class="bg-sky-500 px-10 text-white">Search</button>
+            placeholder="Search by city..." v-model="city" />
+        <button class="bg-sky-500 px-10 text-white" @click="handleSearch">Search</button>
     </div>
 </template>
