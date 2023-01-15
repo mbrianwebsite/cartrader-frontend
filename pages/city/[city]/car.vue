0f35211c927f530d7ab21f6c0152f2180b685fd8
MBrian Website <muhammad.brian.27@gmail.com>
2023-01-15 20:27:42 +0700

    SET UP DYNAMIC TITLE

---
+++ pages/city/[city]/car.vue
@@ -0,0 +1,31 @@
+<script setup>
+const route = useRoute()
+
+useHead({
+    title: `${route.params.make ? toTitleCase(route.params.make) : 'Cars'} in ${toTitleCase(route.params.city)} `
+})
+
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+</script>
+
+<template>
+    <!-- CARS PAGE -->
+    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10  pb-16 w-3/5">
+        <div class="mt-32 flex">
+            <!-- CAR SIDE BAR -->
+            <CarSideBar />
+            <!-- CAR SIDE BAR -->
+
+            <NuxtPage />
+        </div>
+    </div>
+    <!-- CARS PAGE -->
+</template>
