c3a9eea6f927293ca3c0cf4a9dc6b700e3f20b59
MBrian Website <muhammad.brian.27@gmail.com>
2023-01-16 17:26:02 +0700

    SET UP THROWING A CUSTOM SERVER ERROR

--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -15,7 +15,7 @@ const handleSearch = () => {
     <div class="font-serif w-full text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
         :class="cityError ? 'ring-8 ring-red-600' : ''">
         <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
-            placeholder="Search by city..." v-model="city" />
+            placeholder="Search by city..." v-model="city" @keyup.enter="handleSearch" />
         <button class="bg-sky-500 px-10 text-white" @click="handleSearch">Search</button>
     </div>
 </template>
--- error.vue
+++ error.vue
@@ -2,7 +2,9 @@
 const error = useError()
 
 const handleEror = () => {
-    navigateTo("/")
+    clearError({
+        redirect: "/"
+    })
 }
 
 </script>
@@ -11,7 +13,7 @@ const handleEror = () => {
     <div class=" flex h-screen justify-center items-center flex-col">
         <h1 class="text-9xl font-sans font-bold">{{ error.statusCode }}</h1>
         <p class="mt-7 text-4xl font-sans font-bold">{{ error.message }}</p>
-        <button @click="handleEror" to="/"
+        <button @click="handleEror"
             class="mt-7 px-6 py-3 rounded-md bg-blue-400 text-white text-xl font-sans font-medium">Go
             Back</button>
     </div>
