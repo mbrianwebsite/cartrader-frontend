6d5dc168d3430a4bb497da1c0617d1095f2563da
MBrian Website <muhammad.brian.27@gmail.com>
2023-01-16 21:06:53 +0700

    INSTALL NUXTIMAGE MODULE

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -8,7 +8,7 @@ const prop = defineProps({
     <div class="shadow border w-[800px] overflow-hidden mb-5 cursor-pointer h-[200px]"
         @click="navigateTo(`/car/${car.name}-${car.id}`)">
         <div class="flex h-full">
-            <img :src="car.url" alt="" class="w-[300px] h-full" />
+            <nuxt-img :src="car.url" alt="" class="w-[300px] h-full" />
             <div class="p-4 flex flex-col">
                 <div>
                     <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
