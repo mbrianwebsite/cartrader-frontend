d0653fca1bb582ffa29a243255c93b0c15596c30
MBrian Website <muhammad.brian.27@gmail.com>
2023-01-18 19:44:57 +0700

    INSTALL VUEUSE

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/image-edge"],
+  modules: ["@nuxt/image-edge", "@vueuse/nuxt"],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
