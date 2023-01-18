e9a5b61a9f343affdfafa144670291dd0c0e3b1e
MBrian Website <muhammad.brian.27@gmail.com>
2023-01-19 05:36:41 +0700

    CONECTING DATABASE

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/image-edge", "@vueuse/nuxt"],
+  modules: ["@nuxtjs/supabase", "@nuxt/image-edge", "@vueuse/nuxt"],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
