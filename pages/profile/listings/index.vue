43d0ff10f2fa8eb1a431fc324d89889b872690dd
MBrian Website <muhammad.brian.27@gmail.com>
2023-01-19 10:15:25 +0700

    SETUP AUTH MIDDLEWARE

--- pages/profile/listings/index.vue
+++ pages/profile/listings/index.vue
@@ -1,6 +1,9 @@
 <script setup>
 definePageMeta({
   layout: "custom",
+  middleware: [
+    "auth"
+  ]
 });
 
 const { listings } = useCars();
@@ -10,9 +13,7 @@ const { listings } = useCars();
   <div>
     <div class="flex justify-between mt-24 items-center">
       <h1 class="text-6xl">My Listings</h1>
-      <NuxtLink
-        to="/profile/listings/create"
-        class="
+      <NuxtLink to="/profile/listings/create" class="
           w-9
           h-9
           bg-blue-400
@@ -23,16 +24,10 @@ const { listings } = useCars();
           text-white
           font-bold
           cursor-pointer
-        "
-        >+</NuxtLink
-      >
+        ">+</NuxtLink>
     </div>
     <div class="shadow rounded p-3 mt-5">
-      <CarListingCard
-        v-for="listing in listings"
-        :key="listing.id"
-        :listing="listing"
-      />
+      <CarListingCard v-for="listing in listings" :key="listing.id" :listing="listing" />
     </div>
   </div>
 </template>
