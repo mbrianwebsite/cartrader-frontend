1cb13aa0425c4ae2a17d48d160c73beadc64442b
MBrian Website <muhammad.brian.27@gmail.com>
2023-01-25 08:16:29 +0700

    DELETE END POINT FOR MY CAR LISTINGS

--- pages/profile/listings/index.vue
+++ pages/profile/listings/index.vue
@@ -6,9 +6,15 @@ definePageMeta({
   ]
 });
 
-// const { listings } = useCars();
 const user = useSupabaseUser()
-const { data: listings } = await useFetch(`/api/car/listings/user/${user.value.id}`)
+const { data: listings, refresh } = await useFetch(`/api/car/listings/user/${user.value.id}`)
+
+const handleDelete = async (id) => {
+  await $fetch(`/api/car/listings/${id}`, {
+    method: "delete"
+  })
+  listings.value = listings.value.filter((listing) => listing.id !== id)
+}
 
 
 </script>
@@ -31,7 +37,7 @@ const { data: listings } = await useFetch(`/api/car/listings/user/${user.value.i
         ">+</NuxtLink>
     </div>
     <div class="shadow rounded p-3 mt-5">
-      <CarListingCard v-for="listing in listings" :key="listing.id" :listing="listing" />
+      <CarListingCard v-for="listing in listings" :key="listing.id" :listing="listing" @delete-click="handleDelete" />
     </div>
   </div>
 </template>
