fd0676f94459d6a4838f38fcde2260829f18d433
MBrian Website <muhammad.brian.27@gmail.com>
2023-01-25 22:20:25 +0700

    SET UP GET METHOD FOR MESSAGE LIST

--- pages/profile/listings/view/[id].vue
+++ pages/profile/listings/view/[id].vue
@@ -5,13 +5,12 @@ definePageMeta({
     "auth"
   ]
 });
+const route = useRoute()
+const { data: messages } = useFetch(`/api/car/listings/${route.params.id}/message`)
 </script>
 
 <template>
   <div class="rounded shadow mt-20">
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
+    <CarMessageCard v-for="message in messages" :key="message.id" :message="message" />
   </div>
 </template>
